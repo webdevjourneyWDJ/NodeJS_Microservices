5623dd7c6bafb59c141fe2259c8d374b06d6a46e
Gamaliel Barron <gbarronmm@gmail.com>
2020-07-21 12:30:17 -0500

    added log to un-register

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -5,7 +5,7 @@ class ServiceRegistry {
     constructor(log){
         this.log = log;
         this.services = {};
-        this.timeout = 10;
+        this.timeout = 30;
     }
 
     get(name, version){
@@ -38,6 +38,7 @@ class ServiceRegistry {
     unregister(name, version, ip, port){
         const key = name+version+ip+port;
         delete this.services[key];
+        this.log.debug(`Unregistered services ${name}, version ${version} at ${ip}:${port}`);
         return key;
     }
 
